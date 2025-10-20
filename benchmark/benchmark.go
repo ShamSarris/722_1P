7c3052fde4793146aaef2ce5f42de73d4e67dae8
ShamSarris <hamsarris42@gmail.com>
2025-10-20 14:44:44 -0400

    Updated benchmark to work (build) and run

--- benchmark/benchmark.go
+++ benchmark/benchmark.go
@@ -1,14 +1,17 @@
-package benchmark
+package main
 
 import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
 	"time"
 )
+
+// TODO: Fix key generation; Writes can create new key or update old; Reads should only read existing keys
 
 // Helper function to generate random string of given size
 func randomString(size int) string {
@@ -21,11 +24,11 @@ func randomString(size int) string {
 }
 
 func main() {
-	primaryAddr := flag.String("primary", "127:0.0.1:8080", "Address of primary node server to connect to")
-	backupAddr_1 := flag.String("backup1", "127:0.0.1:8083", "Address of first backup node server to connect to")
-	backupAddr_2 := flag.String("backup2", "127:0.0.1:8085", "Address of second backup node server to connect to")
+	primaryAddr := flag.String("primary", "127.0.0.1:8081", "Address of primary node server to connect to")
+	backupAddr_1 := flag.String("backup1", "127.0.0.1:8083", "Address of first backup node server to connect to")
+	backupAddr_2 := flag.String("backup2", "127.0.0.1:8085", "Address of second backup node server to connect to")
 	rwRatio := flag.Float64("rwratio", 0.8, "Ratio of read to write operations (0.0 to 1.0)")
-	writeSize := flag.Int("writesize", 100, "Size of each write operation in bytes")
+	writeSize := flag.Int("writesize", 10, "Size of each write operation in bytes")
 	readFrom := flag.Int("readfromlog", 0, "0 = Read only from primary, 1 = Read only from backups, 2 = Read from both at random")
 	duration := flag.Int("duration", 60, "Duration of the benchmark test in seconds")
 	clients := flag.Int("clients", 1, "Number of concurrent clients to simulate")
@@ -41,7 +44,7 @@ func main() {
 	defer file.Close()
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
-	writer.Write([]string{"Client", "Operation", "Latency"})
+	writer.Write([]string{"Client", "Operation", "Latency", "isFailed"})
 
 	// Benchmark logic
 	endTime := time.Now().Add(time.Duration(*duration) * time.Second)
@@ -52,36 +55,44 @@ func main() {
 			if rand.Float64() < *rwRatio {
 				// WRITE operation
 				operation = "WRITE"
-				url = fmt.Sprintf("http://%s/write?key=%d&value=%s", *primaryAddr, i, randomString(*writeSize))
+				log.Printf("Sending write with key: %d", i)
+				url = fmt.Sprintf("http://%s/%d/%s", *primaryAddr, i, randomString(*writeSize))
 			} else {
 				// READ operation
 				operation = "READ"
 				switch *readFrom {
 				case 0:
-					url = fmt.Sprintf("http://%s/read?key=%d", *primaryAddr, i)
+					log.Printf("Sending read to primary with key: %d", i)
+					url = fmt.Sprintf("http://%s/%d", *primaryAddr, i)
 				case 1:
 					backup := rand.Intn(2)
 					switch backup {
 					case 0:
-						url = fmt.Sprintf("http://%s/read?key=%d", *backupAddr_2, i)
+						log.Printf("Sending read to backup 2 with key: %d", i)
+						url = fmt.Sprintf("http://%s/%d", *backupAddr_2, i)
 					default:
-						url = fmt.Sprintf("http://%s/read?key=%d", *backupAddr_1, i)
+						log.Printf("Sending read to backup 1 with key: %d", i)
+						url = fmt.Sprintf("http://%s/%d", *backupAddr_1, i)
 					}
 				default:
 					backup := rand.Intn(2)
 					switch backup {
 					case 0:
-						url = fmt.Sprintf("http://%s/read?key=%d", *primaryAddr, i)
+						log.Printf("Sending read to primary with key: %d", i)
+						url = fmt.Sprintf("http://%s/%d", *primaryAddr, i)
 					case 1:
-						url = fmt.Sprintf("http://%s/read?key=%d", *backupAddr_1, i)
+						log.Printf("Sending read to backup 1 with key: %d", i)
+						url = fmt.Sprintf("http://%s/%d", *backupAddr_1, i)
 					default:
-						url = fmt.Sprintf("http://%s/read?key=%d", *backupAddr_2, i)
+						log.Printf("Sending read to backup 2 with key: %d", i)
+						url = fmt.Sprintf("http://%s/%d", *backupAddr_2, i)
 					}
 				}
 			}
 
 			startTime := time.Now()
 			resp, err := http.Get(url)
+			var isFailed = false
 			if err != nil {
 				fmt.Println("Error making request:", err)
 				continue
@@ -90,7 +101,7 @@ func main() {
 			latency := time.Since(startTime).Milliseconds()
 
 			// Log latency
-			writer.Write([]string{fmt.Sprintf("%d", i), operation, fmt.Sprintf("%d", latency)})
+			writer.Write([]string{fmt.Sprintf("%d", i), operation, fmt.Sprintf("%d", latency), fmt.Sprintf("%t", isFailed)})
 		}
 	}
 }
